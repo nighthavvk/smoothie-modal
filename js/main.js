88e5ba4ad83e0a6dced92735b83b8eb92ecd6061
Maks <max@vertalab.com>
2016-04-30 11:17:56 +0300

    Js improvements, fixed background

--- js/main.js
+++ js/main.js
@@ -1,128 +1,68 @@
-// var currentScrollTop = 0;
-// var modalWrapper = $('.js-modal-wrapper');
-// var mainContainer = $(window);
-//
-// var getScrollTop = function () {
-//   return mainContainer.scrollTop();
-// };
-//
-// var getItems = function (trigger) {
-//   var mData = $(trigger).data('modal');
-//   var modalItems = {
-//     modalWindow: $('.js-modal-window[data-modal="'+ mData +'"]'),
-//     modalOverlay: $('.js-modal-overlay[data-modal="'+ mData +'"]')
-//   };
-//   return modalItems;
-// };
-//
-// var getNumOfOpenModals = function () {
-//   return parseInt($('.js-modal-window.open').length);
-// };
-//
-// var setModalIndex = function (modalWindow, modalOverlay) {
-//   var index = getNumOfOpenModals();
-//   var currIndex = parseInt(modalWindow.css('z-index'));
-//   modalWindow.css('z-index', currIndex * index);
-//   modalOverlay.css('z-index', currIndex * index - 10);
-// };
-//
-// var isAnyModalOpen = function () {
-//   if (getNumOfOpenModals() === 0) {
-//     return false;
-//   } else {
-//     return true;
-//   }
-// };
-//
-// var modalOpen = function (modalWindow, modalOverlay) {
-//   if (!isAnyModalOpen()) {
-//     currentScrollTop = getScrollTop();
-//   }
-//   modalWindow.addClass('open');
-//   modalOverlay.addClass('open');
-//   setModalIndex(modalWindow, modalOverlay);
-//   modalWrapper.addClass('fixed');
-//   mainContainer.scrollTop(0);
-// };
-//
-// var modalClose = function (modalWindow, modalOverlay) {
-//   modalWindow.removeClass('open');
-//   modalOverlay.removeClass('open');
-//   modalWrapper.removeClass('fixed');
-//   if (!isAnyModalOpen()) {
-//     mainContainer.scrollTop(currentScrollTop);
-//   }
-// };
-//
-// $('.js-modal-open').click(function() {
-//   modalItems = getItems(this);
-//   modalOpen(modalItems.modalWindow, modalItems.modalOverlay);
-// });
-//
-// $('.js-modal-close').click(function() {
-//   modalItems = getItems(this);
-//   modalClose(modalItems.modalWindow, modalItems.modalOverlay);
-// });
-
-var modal = {
-  currentScrollTop: 0,
-  modalWrapper: $('.js-modal-wrapper'),
-  mainContainer: $(window),
-  openTrigger: $('.js-modal-open'),
-  closeTrigger: $('.js-modal-close'),
-  getScrollTop: function () {
-    return this.mainContainer.scrollTop();
-  },
-  getItems: function (trigger) {
-    var mData = $(trigger).data('modal');
-    var modalItems = {
-      modalWindow: $('.js-modal-window[data-modal="'+ mData +'"]'),
-      modalOverlay: $('.js-modal-overlay[data-modal="'+ mData +'"]')
-    };
-    return modalItems;
-  },
-  getNumOfOpenModals: function () {
-    return parseInt($('.js-modal-window.open').length);
-  },
-  setModalIndex: function (modalWindow, modalOverlay) {
-    var index = this.getNumOfOpenModals();
-    var currIndex = parseInt(modalWindow.css('z-index'));
-    modalWindow.css('z-index', currIndex * index);
-    modalOverlay.css('z-index', currIndex * index - 10);
-  },
-  isAnyModalOpen: function () {
-    if (this.getNumOfOpenModals() === 0) {
-      return false;
-    } else {
-      return true;
+(function($){
+  var smoothieModal = {
+    currentScrollTop: 0,
+    $modalWrapper: $('.js-modal-wrapper'),
+    wrapperTop: $('.js-modal-wrapper').css('top'),
+    $mainContainer: $(window),
+    $openTrigger: $('.js-modal-open'),
+    $closeTrigger: $('.js-modal-close'),
+    getScrollTop: function () {
+      return this.$mainContainer.scrollTop();
+    },
+    getItems: function (trigger) {
+      var mData = $(trigger).data('modal');
+      var modalItems = {
+        modalWindow: $('.js-modal-window[data-modal="'+ mData +'"]'),
+        modalOverlay: $('.js-modal-overlay[data-modal="'+ mData +'"]')
+      };
+      return modalItems;
+    },
+    getNumOfOpenModals: function () {
+      return parseInt($('.js-modal-window.open').length);
+    },
+    setModalIndex: function (modalWindow, modalOverlay) {
+      var index = this.getNumOfOpenModals();
+      var currIndex = parseInt(modalWindow.css('z-index'));
+      modalWindow.css('z-index', currIndex * index);
+      modalOverlay.css('z-index', currIndex * index - 10);
+    },
+    isAnyModalOpen: function () {
+      if (this.getNumOfOpenModals() === 0) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+    open: function (modalWindow, modalOverlay) {
+      if (!this.isAnyModalOpen()) {
+        currentScrollTop = this.getScrollTop();
+      }
+      modalWindow.addClass('open');
+      modalOverlay.addClass('open');
+      this.setModalIndex(modalWindow, modalOverlay);
+      this.$modalWrapper.addClass('fixed');
+      this.wrapperTop = this.$modalWrapper.css('top');
+      this.$modalWrapper.css('top', -currentScrollTop);
+      this.$mainContainer.scrollTop(0);
+    },
+    close: function (modalWindow, modalOverlay) {
+      modalWindow.removeClass('open');
+      modalOverlay.removeClass('open');
+      if (!this.isAnyModalOpen()) {
+        this.$modalWrapper.removeClass('fixed');
+        this.$modalWrapper.css('top', this.wrapperTop);
+        this.$mainContainer.scrollTop(currentScrollTop);
+      }
     }
-  },
-  open: function (modalWindow, modalOverlay) {
-    if (!this.isAnyModalOpen()) {
-      currentScrollTop = this.getScrollTop();
-    }
-    modalWindow.addClass('open');
-    modalOverlay.addClass('open');
-    this.setModalIndex(modalWindow, modalOverlay);
-    this.modalWrapper.addClass('fixed');
-    this.mainContainer.scrollTop(0);
-  },
-  close: function (modalWindow, modalOverlay) {
-    modalWindow.removeClass('open');
-    modalOverlay.removeClass('open');
-    this.modalWrapper.removeClass('fixed');
-    if (!this.isAnyModalOpen()) {
-      this.mainContainer.scrollTop(currentScrollTop);
-    }
-  }
-};
+  };
 
-$(modal.openTrigger).click(function(){
-  modalItems = modal.getItems(this);
-  modal.open(modalItems.modalWindow, modalItems.modalOverlay);
-});
+  smoothieModal.$openTrigger.click(function(){
+    modalItems = smoothieModal.getItems(this);
+    smoothieModal.open(modalItems.modalWindow, modalItems.modalOverlay);
+  });
 
-$(modal.closeTrigger).click(function(){
-  modalItems = modal.getItems(this);
-  modal.close(modalItems.modalWindow, modalItems.modalOverlay);
-});
+  smoothieModal.$closeTrigger.click(function(){
+    modalItems = smoothieModal.getItems(this);
+    smoothieModal.close(modalItems.modalWindow, modalItems.modalOverlay);
+  });
+})(jQuery);
