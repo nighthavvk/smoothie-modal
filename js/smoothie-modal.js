e65b0a3fe8a938b40d4df80ede018669d2fba918
maxcharuk <maxcharuk@gmail.com>
2016-05-05 03:40:23 +0300

    Landing page improvements

--- js/smoothie-modal.js
+++ js/smoothie-modal.js
@@ -1,4 +1,4 @@
-(function($){
+(function($) {
   var smoothieModal = {
     currentScrollTop: 0,
     wrapperTop: 0,
@@ -6,41 +6,41 @@
     $mainContainer: $(window),
     $openTrigger: $('.js-modal-open'),
     $closeTrigger: $('.js-modal-close'),
-    getScrollTop: function () {
+    getScrollTop: function() {
       return this.$mainContainer.scrollTop();
     },
-    getItems: function (trigger) {
+    getItems: function(trigger) {
       var mData = $(trigger).data('modal');
       var modalItems = {
-        modalWindow: $('.js-modal-window[data-modal="'+ mData +'"]'),
-        modalOverlay: $('.js-modal-overlay[data-modal="'+ mData +'"]')
+        modalWindow: $('.js-modal-window[data-modal="' + mData + '"]'),
+        modalOverlay: $('.js-modal-overlay[data-modal="' + mData + '"]')
       };
       return modalItems;
     },
-    getNumOfOpenModals: function () {
+    getNumOfOpenModals: function() {
       return parseInt($('.js-modal-window.smoothie-modal-open').length);
     },
-    setModalIndex: function (modalWindow, modalOverlay) {
+    setModalIndex: function(modalWindow, modalOverlay) {
       var index = this.getNumOfOpenModals();
       var currIndex = parseInt(modalWindow.css('z-index'));
       modalWindow.css('z-index', currIndex * index);
       modalOverlay.css('z-index', currIndex * index - 10);
     },
-    isAnyModalOpen: function () {
+    isAnyModalOpen: function() {
       if (this.getNumOfOpenModals() === 0) {
         return false;
       } else {
         return true;
       }
     },
-    isModalOutOfBounds: function (modalWindow) {
+    isModalOutOfBounds: function(modalWindow) {
       if (modalWindow.outerHeight() > this.$mainContainer.outerHeight()) {
         return true;
       } else {
         return false;
       }
     },
-    open: function (modalWindow, modalOverlay) {
+    open: function(modalWindow, modalOverlay) {
       if (!this.isAnyModalOpen()) {
         currentScrollTop = this.getScrollTop();
         wrapperTop = $('.js-modal-wrapper').css('top');
@@ -52,7 +52,7 @@
       this.$modalWrapper.css('top', -currentScrollTop);
       this.$mainContainer.scrollTop(0);
     },
-    close: function (modalWindow, modalOverlay) {
+    close: function(modalWindow, modalOverlay) {
       modalWindow.removeClass('smoothie-modal-open');
       modalOverlay.removeClass('smoothie-modal-open');
       if (!this.isAnyModalOpen()) {
@@ -63,13 +63,15 @@
     }
   };
 
-  smoothieModal.$openTrigger.click(function(){
-    modalItems = smoothieModal.getItems(this);
-    smoothieModal.open(modalItems.modalWindow, modalItems.modalOverlay);
-  });
+  $(document).ready(function() {
+    smoothieModal.$openTrigger.click(function() {
+      modalItems = smoothieModal.getItems(this);
+      smoothieModal.open(modalItems.modalWindow, modalItems.modalOverlay);
+    });
 
-  smoothieModal.$closeTrigger.click(function(){
-    modalItems = smoothieModal.getItems(this);
-    smoothieModal.close(modalItems.modalWindow, modalItems.modalOverlay);
+    smoothieModal.$closeTrigger.click(function() {
+      modalItems = smoothieModal.getItems(this);
+      smoothieModal.close(modalItems.modalWindow, modalItems.modalOverlay);
+    });
   });
 })(jQuery);
